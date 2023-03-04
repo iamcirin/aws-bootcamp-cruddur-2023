84ce8c823f08df2f3cada94dfe77d73ef4cff059
iamcirin <cirine7xu@gmail.com>
2023-03-04 22:08:38 +0000

    implementing rollbar

--- backend-flask/services/notifications_activities.py
+++ backend-flask/services/notifications_activities.py
@@ -1,17 +1,17 @@
 # added for x-eay segment
-from aws_xray_sdk.core import xray_recorder
-from aws_xray_sdk.core import patch_all
+# from aws_xray_sdk.core import xray_recorder
+# from aws_xray_sdk.core import patch_all
 from datetime import datetime, timedelta, timezone
-patch_all()
+# patch_all()
 
 
 # added for x-ray segment
-xray_recorder.begin_segment('notification-activities')
+# xray_recorder.begin_segment('notification-activities')
 
 class NotificationsActivities:
   def run():
   # added this line for x-ray sbsegment
-    with xray_recorder.in_subsegment('notification-activities-subsegment'):
+    # with xray_recorder.in_subsegment('notification-activities-subsegment'):
       now = datetime.now(timezone.utc).astimezone()
       results = [{
         'uuid': '68f126b0-1ceb-4a33-88be-d90fa7109eee',
@@ -34,10 +34,10 @@ class NotificationsActivities:
         }],
       }
       ]
-      xray_recorder.put_metadata('results', len(results))
-      xray_recorder.put_annotation('handle', results[0]['handle'])
-      xray_recorder.end_subsegment()
+      # xray_recorder.put_metadata('results', len(results))
+      # xray_recorder.put_annotation('handle', results[0]['handle'])
+      # xray_recorder.end_subsegment()
       return results
 
 
-xray_recorder.end_segment()
+# xray_recorder.end_segment()
